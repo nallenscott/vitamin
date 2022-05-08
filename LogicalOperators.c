dcf18401e628487ebff4b5ff169a451520a2b493
Nathan Allen <nallenscott@me.com>
2022-05-08 07:13:33 -0400

    Update logical operators

--- LogicalOperators.c
+++ LogicalOperators.c
@@ -3,15 +3,31 @@
 
 int main () {
 
-  // logical operators = && (AND) checks if two conditions are true
-
   float temp = 25;
   bool sunny = true;
 
-  if (temp >= 0 && temp <= 30 && sunny) {
-    printf("The weather is good!\n");
+  // logical operators = && (AND) checks if two conditions are true
+
+  // if (temp >= 0 && temp <= 30 && sunny) {
+  //   printf("The weather is good!\n");
+  // } else {
+  //   printf("The weather is bad!\n");
+  // }
+
+  // logical operators = || (OR) checks if one of two conditions is true
+
+  // if (temp <= 0 || temp >= 30) {
+  //   printf("The weather is bad!\n");
+  // } else {
+  //   printf("The weather is good!\n");
+  // }
+
+  // logical operators = ! (NOT) reverses the state of a condition
+
+  if (!sunny) {
+    printf("It's cloudy outside!\n");
   } else {
-    printf("The weather is bad!\n");
+    printf("It's sunny outside!\n");
   }
 
   return 0;
