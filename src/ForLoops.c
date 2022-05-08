067df58995656bc7208e6cd50f9c27dc2419418c
Nathan <nallenscott@me.com>
2022-05-08 18:07:53 -0400

    Add for loops

---
+++ src/ForLoops.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+  // for loop = repeats a section of code a limited amount of times
+
+  for (int i = 0; i < 10; i+=2) {
+    printf("%d\n", i);
+  }
+
+  return 0;
+}
