183e372bff39e002a7c560b4bf7377988974a97c
Nathan Allen <nallenscott@me.com>
2022-05-10 06:25:02 -0400

    Add 2d arrays

---
+++ src/2DArrays.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main () {
+
+  // 2D array = an array, where each element is an entire array
+  //            useful if you need a matrix, grid, or table of data
+
+  // Delcare a 2D array of ints
+
+  int numbers[3][3] = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+  };
+
+  // OR you can declare it like this:
+
+  // int numbers[2][3];
+
+  // numbers[0][0] = 1;
+  // numbers[0][1] = 2;
+  // numbers[0][2] = 3;
+  // numbers[1][0] = 4;
+  // numbers[1][1] = 5;
+  // numbers[1][2] = 6;
+
+  int rows = sizeof(numbers) / sizeof(numbers[0]);
+  int columns = sizeof(numbers[0]) / sizeof(numbers[0][0]);
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      printf("%d ", numbers[i][j]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
