a74efbb3f07854b18a56536c3c058539a0093f31
Nathan Allen <nallenscott@me.com>
2022-05-10 05:59:36 -0400

    Print array with loop

--- src/Arrays.c
+++ src/Arrays.c
@@ -4,15 +4,21 @@ int main () {
 
   // array = a data structure that can store many values of the same data type
 
-  double prices[10] = {1.23, 2.34, 3.45, 4.56, 5.67}; // array of 5 doubles
+  double prices[] = {1.23, 2.34, 3.45, 4.56, 5.67}; // array of 5 doubles
 
-  prices[0] = 1.11; // change the first value in the array
-  prices[1] = 2.22;
-  prices[2] = 3.33;
-  prices[3] = 4.44;
-  prices[4] = 5.55;
+  // printf("%lu bytes\n", sizeof(prices)); // prints out the size of the array in bytes
 
-  printf("$%.2lf\n", prices[0]); // print the first value in the array
+  // prices[0] = 1.11; // change the first value in the array
+  // prices[1] = 2.22;
+  // prices[2] = 3.33;
+  // prices[3] = 4.44;
+  // prices[4] = 5.55;
+
+  // printf("The first price is %lf\n", prices[0]); // print the first value in the array
+
+  for (int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++) {
+    printf("$%.2f\n", prices[i]); // print each value in the array
+  }
 
   return 0;
 }
